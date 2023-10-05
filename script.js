910b6c430290a28e466f436fdeb0e769b3d959bc
Arthur Bais <artb@tutamail.com>
2023-10-05 20:34:00 +0300

    Refactoring code to use class instead of plain constructors

--- script.js
+++ script.js
@@ -10,107 +10,111 @@ const imageUrlInput = document.getElementById("imgUrlInput");
 const ifReadInput = document.querySelectorAll("input[name='ifRead']");
 
 
-function Book(title, author, pages, isRead = false, coverImgUrl) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.isRead = isRead;
-  if(coverImgUrl) this.coverImgUrl = coverImgUrl;
-}
-
-Book.prototype.toggleRead = function() {
-  this.isRead = !this.isRead;
-}
-
-
-function Library() {
-  this.books = [];
-}
-
-Library.prototype.addBook = function(book) {
-  // let newBook = new Book(title, author, pages, isRead, coverImgUrl);
-  // let newBookId = this.books.length;
-  this.books.push(book);
-
-  // return new book id
-  return this.books.length - 1;
-}
-
-Library.prototype.removeBook = function(bookId) {
-  this.books.splice(bookId, 1);
-}
-
-function DisplayController(){
-  this.library = new Library();
-}
-
-DisplayController.prototype.drawLibrary = function() {
-  bookContainerElement.replaceChildren();
-
-  this.library.books.forEach((book, index) => {
-    this.drawBook(book, index);
-  });
-}
-
-DisplayController.prototype.drawBook = function(book, index) {
-  let bookElement = document.createElement("div");
-  bookElement.className = "bookContainer";
-  bookElement.dataset.indexNumber = index;
-
-  let authorElement, titleElement, pageNum, isRead, coverImg;
-
-  coverImg = document.createElement("img");
-  if(book.coverImgUrl) coverImg.src = book.coverImgUrl;
-  else coverImg.src = "./nocover.png";
-  coverImg.className = "coverImage"
-  bookElement.appendChild(coverImg);
-
-  titleElement = document.createElement("h2");
-  titleElement.innerHTML = book.title;
-  bookElement.appendChild(titleElement);
-
-  authorElement = document.createElement("p");
-  authorElement.innerHTML = `By ${book.author}`;
-  bookElement.appendChild(authorElement);
-
-  pageNum = document.createElement("p");
-  pageNum.innerHTML = `Pages: ${book.pages}`;
-  pageNum.className = "pages";
-  bookElement.appendChild(pageNum);
-  
-  isRead = document.createElement("input");
-  isRead.className  = "readToggle";
-  isRead.type = "checkbox";
-  isRead.checked = book.isRead;
-  if(book.isRead) isRead.innerHTML = "read";
-isRead.addEventListener("click", () => {
-    book.toggleRead();
-    isRead.innerHTML = book.isRead ? "Read" : "Unread";
-  });
-  bookElement.appendChild(isRead);
-
-  let removeBookElement = document.createElement("button");
-  removeBookElement.className = "removeButton";
-  removeBookElement.addEventListener("click", () => display.removeBook(index));
-  bookElement.appendChild(removeBookElement);
-  bookContainerElement.appendChild(bookElement);
-}
-
-DisplayController.prototype.removeBook = function(bookId) {
-  this.library.removeBook(bookId)
-  let bookElement;
-  for (let book of bookContainerElement.children) {
-    if(parseInt(book.dataset.indexNumber) === bookId)
-      bookElement = book;
+class Book {
+  constructor(title, author, pages, isRead = false, coverImgUrl) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.isRead = isRead;
+    if(coverImgUrl) this.coverImgUrl = coverImgUrl;
   }
 
-  // remove element with fadeout animation, took from here:
-  // https://stackoverflow.com/a/33424474
-  bookElement.style.transition = "opacity 0.8s ease";
-  bookElement.style.opacity = 0;
-  setTimeout(function(){
-    bookContainerElement.removeChild(bookElement);
-  }, 800)
+  toggleRead = function() {
+    this.isRead = !this.isRead;
+  }
+}
+
+
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(book) {
+    this.books.push(book);
+
+    // return new book id
+    return this.books.length - 1;
+  }
+
+  removeBook(bookId) {
+    this.books.splice(bookId, 1);
+  }
+}
+
+class DisplayController {
+  constructor() {
+    this.library = new Library();
+  }
+  drawLibrary() {
+    bookContainerElement.replaceChildren();
+
+    this.library.books.forEach((book, index) => {
+      this.drawBook(book, index);
+    });
+  }
+
+  drawBook(book, index) {
+    let bookElement = document.createElement("div");
+    bookElement.className = "bookContainer";
+    bookElement.dataset.indexNumber = index;
+
+    let authorElement, titleElement, pageNum, isRead, coverImg;
+
+    coverImg = document.createElement("img");
+    if(book.coverImgUrl) coverImg.src = book.coverImgUrl;
+    else coverImg.src = "./nocover.png";
+    coverImg.className = "coverImage"
+    bookElement.appendChild(coverImg);
+
+    titleElement = document.createElement("h2");
+    titleElement.innerHTML = book.title;
+    bookElement.appendChild(titleElement);
+
+    authorElement = document.createElement("p");
+    authorElement.innerHTML = `By ${book.author}`;
+    bookElement.appendChild(authorElement);
+
+    pageNum = document.createElement("p");
+    pageNum.innerHTML = `Pages: ${book.pages}`;
+    pageNum.className = "pages";
+    bookElement.appendChild(pageNum);
+    
+    isRead = document.createElement("input");
+    isRead.className  = "readToggle";
+    isRead.type = "checkbox";
+    isRead.checked = book.isRead;
+    if(book.isRead) isRead.innerHTML = "read";
+  isRead.addEventListener("click", () => {
+      book.toggleRead();
+      isRead.innerHTML = book.isRead ? "Read" : "Unread";
+    });
+    bookElement.appendChild(isRead);
+
+    let removeBookElement = document.createElement("button");
+    removeBookElement.className = "removeButton";
+    removeBookElement.addEventListener("click", () => display.removeBook(index));
+    bookElement.appendChild(removeBookElement);
+    bookContainerElement.appendChild(bookElement);
+  }
+
+  // need a separate method for removing books from display as well as from internal library
+  removeBook(bookId) {
+    this.library.removeBook(bookId)
+    let bookElement;
+    for (let book of bookContainerElement.children) {
+      if(parseInt(book.dataset.indexNumber) === bookId)
+        bookElement = book;
+    }
+
+    // remove element with fadeout animation, took from here:
+    // https://stackoverflow.com/a/33424474
+    bookElement.style.transition = "opacity 0.8s ease";
+    bookElement.style.opacity = 0;
+    setTimeout(function(){
+      bookContainerElement.removeChild(bookElement);
+    }, 800)
+  }
 }
 
 let display = new DisplayController();
